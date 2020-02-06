35b46fd284ef86f9cf8857ea58446e176aeada63
Yingying Xia <yxia@yingyings-mbp.attlocal.net>
2020-02-05 20:43:28 -0800

    another commit

--- helloworld.ex
+++ helloworld.ex
@@ -19,6 +19,12 @@ defmodule M do
     list_stuff()
     map_stuff()
     pattern_matching_stuff()
+    anoynomous_function_stuff()
+    recursion_stuff()
+    enumerator()
+    exception_handle()
+    concurrency()
+    
   end
 
   def data_stuff do
@@ -177,27 +183,27 @@ defmodule M do
     end
 
      #display_list(words)
-     # delete element
-     IO.puts display_list(List.delete(words, "Random"))
-     # delete by index
-     IO.puts display_list(List.delete_at(words, 1))
+    # delete element
+    #  IO.puts display_list(List.delete(words, "Random"))
+    #  # delete by index
+    #  IO.puts display_list(List.delete_at(words, 1))
 
-     IO.puts display_list(List.insert_at(words, 4, "yeah"))
-     IO.puts List.first(words)
-     IO.puts List.last(words)
+    #  IO.puts display_list(List.insert_at(words, 4, "yeah"))
+    #  IO.puts List.first(words)
+    #  IO.puts List.last(words)
 
      my_stats = [name: "YY", height: 2.2]
   
   end
 
   ## prolog
-  def display_list ([word|words]) do
-      IO.puts word
-      display_list(words)
-  end
+  # def display_list ([word|words]) do
+  #     IO.puts word
+  #     display_list(words)
+  # end
 
   ## base case : do nothing
-  def display_list([]), do: nil
+  ##def display_list([]), do: nil
 
 
   def map_stuff do
@@ -219,13 +225,118 @@ defmodule M do
 
   def pattern_matching_stuff do
     ###39:38
+    [length, width] = [20,30]
+    IO.puts "Width : #{width}"
+  end
+  
+  def anoynomous_function_stuff do
+    get_sum = fn(x,y) -> x + y end
+    IO.puts "5 + 5 = #{get_sum.(5,5)}"
+
+    get_less = &(&1 - &2) ##subtract values
+    IO.puts "7 - 6 = #{get_less.(7,6)}"
+
+    add_sum = fn
+      {x,y} -> IO.puts "#{x} + #{y} = #{x+y}"
+      {x,y,z} -> IO.puts "#{x} + #{y} + #{z}= #{x+y+z}"
+    end
+
+    add_sum.({2,99})
+    add_sum.({2,99,1})
+
+    IO.puts do_it()
+
+
+  end 
+
+  def do_it(x \\ 1, y \\ 1) do
+    x + y
   end
 
+  ## calls factorial
+  def recursion_stuff do
+
+    IO.puts "Factorial of 4 : #{factorial(4)}"
+    IO.puts "Sum : #{sum([1,2,3,4])}"
+
+    loop(5,1)
+    
+  end
+
+  ##factorial function 
+  def factorial(num) do
+    if num <= 1 do
+      1
+    else
+      result = num * factorial(num-1)
+      result
+    end
+  end
+
+  def loop(0,_), do: nil
+  def loop(max, min) do
+    if max < min do
+      loop(0, min)
+    else
+      IO.puts "Num : #{max}"
+      loop(max-1, min)
+    end
+  end
+
+  def sum([]), do: 0
+ 
+  ## if(some_condition?, do: :this, else: :that)
+  def sum([h|t]), do: h + sum(t)
+  
+  def enumerator do
+    IO.puts "Even list: #{Enum.all?([1,2,3],
+    fn(n) -> rem(n,2)==0 end)}"
+
+    IO.puts "Even list: #{Enum.any?([1,2,3],
+    fn(n) -> rem(n,2)==0 end)}"
+
+    dbl_list = Enum.map([1,2,3], fn(n)-> n * 2 end)
+
+    sum_vals = Enum.reduce([1,2,9], fn(n,sum)-> n + sum end)
+
+    IO.inspect Enum.uniq([1,2,2])
+
+    dbl_list_values = for n <- [1,2,3], do: n*2
+    IO.inspect dbl_list_values
+    even_list = for n <- [1,2,3,4], rem(n,2)==0, do: n
+    IO.inspect even_list
+  end
+
+  ## exception handling
+  def exception_handle do
+    err = try do
+      5 / 0
+    rescue 
+      ArithmeticError -> "Can't divide by zero"
+    end
+    IO.puts err
+  end
+
+  ## concurrency! multiple blocks of code all at the once
+  def concurrency do
+    spawn(fn() -> loop(50,1) end)
+    spawn(fn() -> loop(100,1) end)
+
+    send(self(),{:french, "BOB"})
+    receive do
+      {:german, name} -> IO.puts "Guten tag #{name}"
+      {:french, name} -> IO.puts "Bonjour #{name}"
+      {:chinese, name} -> IO.puts "Ni hao #{name}"
+      {:english, name} -> IO.puts "hello #{name}"
 
 
+      ##timeout if no response ??
+    after
+      500 -> IO.puts "TIMES UP"
+    end
 
-
-
+    
+  end
 end
 
 
